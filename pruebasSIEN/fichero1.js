4e0a8530020d39c46dc2bfe22cb0f8604d8b2800
Ender-osur <ender.ga10@gmail.com>
2021-07-12 17:14:26 -0500

    Aquí está la página

--- pruebasSIEN/fichero1.js
+++ pruebasSIEN/fichero1.js
@@ -1,5 +1,5 @@
-alert("Bienvenido a Javascript")
+/*alert("Bienvenido a Javascript")
 var nombre=prompt("¿Cómo te llamas?")
 var texto ="Hola, "+nombre+". Me alegro de conocerte. "
 alert(texto)
-console.log(texto)
+console.log(texto)*/
